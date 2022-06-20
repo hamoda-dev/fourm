841f7872126246ff35e3284380c6ae613a1651ac
Mohammed Hamid Hamoda <hamoda.dev@gmail.com>
2022-06-20 02:51:19 +0200

    fix policy to static analizer

--- app/Policies/ThreadPolicy.php
+++ app/Policies/ThreadPolicy.php
@@ -11,52 +11,6 @@ class ThreadPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Thread  $thread
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Thread $thread)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Thread  $thread
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Thread $thread)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can delete the model.
      *
      * @param  \App\Models\User  $user
@@ -66,29 +20,5 @@ class ThreadPolicy
     public function delete(User $user, Thread $thread)
     {
         return $user->id == $thread->user_id;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Thread  $thread
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Thread $thread)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Thread  $thread
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Thread $thread)
-    {
-        //
     }
 }
